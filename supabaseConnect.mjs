d504b2c8b989bbcc1e025c3cb3581a61b1daa0da
Yossi Weinberger <137178593+yossi-weinberger@users.noreply.github.com>
2024-09-03 00:51:35 +0300

    fix production

--- supabaseConnect.mjs
+++ supabaseConnect.mjs
@@ -4,18 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY; // שימוש במפתח Service Role
+const supabaseKey = process.env.SUPABASE_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Missing Supabase credentials");
 }
 
-const supabase = createClient(supabaseUrl, supabaseKey, {
-  auth: {
-    autoRefreshToken: false,
-    persistSession: false,
-  },
-});
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export async function checkSupabaseConnection() {
   try {
